bcd72d579c31fcb3d2f1767a39550fdebc299215
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-09-27 21:37:12 -0400

    extra formatting, added notes to continue work

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer'
-<link rel="stylesheet" href="./assets/social-share-kit-master/dist/css/social-share-kit.css" type="text/css"></link>
-
-
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,8 @@
 export default function Resume() {
-    return (
-      <div className="d-flex align-items-center flex-column">
-        <h1 className="">Resume Page</h1>
-        <a href = "../assets/resume.pdf"
-      Download = "resume.pdf">
-         <button type = "button"> Download My Resume </button>
-      </a>      </div>
-    );
-  }
+  return (
+    <div className="d-flex align-items-center flex-column">
+      <h1 className="">Resume Page</h1>
+      <a className="btn btn-primary btn-l" href="/assets/resume.pdf" Download="resume.pdf">Download my resume!</a>
+    </div>
+  );
+}
