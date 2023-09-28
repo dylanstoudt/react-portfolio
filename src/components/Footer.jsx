02a2570f0c18ee45256ba72b7e8ca971005c9315
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-09-27 21:16:15 -0400

    added reume download button, is broken, fixed formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import Badge from 'react-bootstrap/Badge';
 // <!-- Social Share Kit CSS -->
 export default function Footer() {
     return(
-        <footer className="text-center text-light fixed-bottom bg-dark" style={{height: "200px"}}>
+        <footer className="text-center text-light fixed-bottom bg-dark" style={{height: "120px"}}>
             <h1 className="pt-3">Created with ❤️ by Dylan Stoudt</h1>
             <ul className="d-flex justify-content-around pt-1">
-            <Badge pill bg="primary">
+            {/* <Badge pill bg="primary"> */}
             <a className="link-secondary" href="https://github.com/dylanstoudt">Github</a>
-            </Badge>
-            <a href="https://www.linkedin.com/in/dylan-stoudt/">LinkedIn</a>
-            <a className="ssk ssk-twitter" href="mailto: dylanstoudt@aol.com">Email</a>
+            {/* </Badge> */}
+            <a className="link-secondary" href="https://www.linkedin.com/in/dylan-stoudt/">LinkedIn</a>
+            <a className="link-secondary" href="mailto: dylanstoudt@aol.com">Email</a>
             </ul>
         </footer>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,9 +12,9 @@ function Nav() {
           to="/"
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === '/Aboutme' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
         >
-          Home
+          About Me
         </Link>
       </li>
       <li className="nav-item">
